2a7939082339f81fc8299e9a8a2db8ae67194f3d
Nikita <nurofen38964@gmail.com>
2016-12-03 22:18:17 +0500

    Add files via upload

--- potrus/js/js.js
+++ potrus/js/js.js
@@ -1,4 +1,27 @@
-﻿var options = {
+﻿//header
+var options = {
   offset: 500
 }
 var header = new Headhesive('.header');
+
+//footer
+/*$(function() 
+{ 
+	$("body").css({padding:0,margin:0});
+	  var f = function() 
+	  {
+	    $(".wrapper").css({position:"relative"});
+	    var h1 = $("body").height();
+	    var h2 = $(window).height();
+	    var d = h2 - h1;
+	    var h = $(".wrapper").height() + d;    
+	    var ruler = $("footer").appendTo(".wrapper");       
+	    h = Math.max(ruler.position().top,h);
+	    ruler.remove();    
+	    $(".wrapper").height(h);
+	  };
+	  setInterval(f,1000);
+	  $(window).resize(f);
+	  f();
+ });*/
+ 
